a3add4210df91f10ade5217212f1601283ad75bb
nicky-code <aline.nicole7@gmail.com>
2019-09-26 18:01:57 +0200

    first commit.

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+
+    QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}

26cd1688f8374f00dbe3e1c14349afdf1dbd5a75
nicky-code <aline.nicole7@gmail.com>
2019-09-27 11:46:47 +0200

    added navbar file.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,32 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
+from flask_login import login_user
+from ..models import Writer
+from .forms import LoginForm,RegistrationForm
+from .. import db
 
 @auth.route('/login')
 def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        writer = Writer.query.filter_by(email = login_form.email.data).first()
+        if writer is not None and writer.verify_password(login_form.password.data):
+            login_user(writer,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "blog login"
+    
     return render_template('auth/login.html')
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        writer = Writer(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(writer)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
